dd11fb0c313b2c7fc92fbfa5176a695f2e9623ea
Teniola <fatunmbiteniola001@gmail.com>
2021-06-12 18:14:23 +0100

    modified render function

--- src/ShowMovie.tsx
+++ src/ShowMovie.tsx
@@ -1,20 +1,31 @@
 import React from "react";
-import Movie from "./Movie";
 
 interface movieProps {
     movies: any
-};
+}
+
 
 const ShowMovie: React.FC<movieProps> = ({movies}) => {
+    const movieDetails = [
+        movies.Title,
+        movies.Director,
+        movies.Writer,
+        movies.stars,
+        movies.Released,
+        movies.Rated,
+    ]
     return(
-        <div>
-        {
-            movies.map((movie: any) => {
-                return <Movie movies={movie} />
-            })
-        }
-        </div>
-    );
-};
+            <div className="movie">
+                <img src={movies.Poster} className="img" width="300px"/>
+                <div className="movie-details">
+                    {
+                        movieDetails.map((detail, i) => {
+                            return <p key={i}> {detail}</p> 
+                        })
+                    }
+                </div>
+            </div>
+        )
+}
 
 export default ShowMovie;
