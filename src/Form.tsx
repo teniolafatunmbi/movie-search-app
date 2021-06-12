ad835355e2677b53ed05db785ce218ea4098cd04
Teniola <fatunmbiteniola001@gmail.com>
2021-06-12 21:30:59 +0100

    fixed bugs

--- src/Form.tsx
+++ src/Form.tsx
@@ -16,8 +16,12 @@ const Form: React.FC<searchProps> = ({Text, setText, setMovies}) => {
     const handleSubmit = async (e: any) => {
         e.preventDefault();
         const res = await axios.get(`http://www.omdbapi.com/?t=${Text}&apikey=6070c157`);
-        if(res.data.Response === "True") setMovies(res.data);
-        else alert(res.data.Error)
+        if(res.data.Response === "True") {
+            setMovies(res.data);
+        }
+        else {
+            alert("Input a movie name");
+        }
     }
 
     return(        
--- src/ShowMovie.tsx
+++ src/ShowMovie.tsx
@@ -6,19 +6,13 @@ interface movieProps {
 
 
 const ShowMovie: React.FC<movieProps> = ({movies}) => {
-    const movieDetails = [
-        movies.Title,
-        movies.Director,
-        movies.Writer,
-        movies.stars,
-        movies.Released,
-        movies.Rated,
-    ]
+   const { Title, Director, Writer, stars, Released, Rated } = movies; 
+    const movieDetails = [ Title, Director, Writer, stars, Released, Rated ];
     return(
             <div className="movie">
                 <img src={movies.Poster} className="img" width="300px"/>
                 <div className="movie-details">
-                    {
+                    {   
                         movieDetails.map((detail, i) => {
                             return <p key={i}> {detail}</p> 
                         })
